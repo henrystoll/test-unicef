3b9d0427de29ddfdadfb8948b94e48423f0cff9e
Henry Stoll <info.henry.stoll@gmail.com>
2022-01-06 12:14:11 +0100

    csrf protection with csurf

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './app.module'
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 import { ValidationPipe } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
 import { PrismaModel } from './_gen/prisma-class'
 import {
   FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
+  NestFastifyApplication
+} from "@nestjs/platform-fastify";
+import fastifyCsrf from "fastify-csrf";
 
 declare const module: any
 
@@ -27,23 +28,27 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Last Mile Monitoring Application')
     .setDescription(
-      'The LMM API description. Download [OpenAPI v3 Specification JSON](http://localhost:3000/api-json)',
+      "The LMM API description. Download [OpenAPI v3 Specification JSON](http://localhost:3000/api-json)"
     )
-    .setVersion('0.1')
+    .setVersion("0.1")
     // .addServer('HTTPS')
     .build()
   const document = SwaggerModule.createDocument(app, config, {
-    extraModels: [...PrismaModel.extraModels],
-  })
-  SwaggerModule.setup('api', app, document)
+    extraModels: [...PrismaModel.extraModels]
+  });
+  SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000)
+  // csrf protection with csurf
+  app.register(fastifyCsrf);
+
+  await app.listen(3000);
 
   // add hot reloading
   if (module.hot) {
-    module.hot.accept()
-    module.hot.dispose(() => app.close())
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
   }
+
   // add prisma service shutdown hooks: https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
   const prismaService: PrismaService = app.get(PrismaService)
   prismaService.enableShutdownHooks(app)
